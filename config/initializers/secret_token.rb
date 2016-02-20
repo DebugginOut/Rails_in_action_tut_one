cd588f4bd65cdc95406c807934f0372c2a63f7a0
DebugginOut <outdebuggin@gmail.com>
2016-02-20 16:55:27 -0600

    new rails project, generated scaffolding for purchases, name and cost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsIBought::Application.config.secret_key_base = '28972ae2debf99822e2984439bd2022ea8df0c58d6849e624eb69678c9d4a05c72e7eff1cf2078a02fee90c9e1cfea75ef3143488c5bf766da99d73fd2d81cb9'
